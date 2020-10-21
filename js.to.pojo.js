8886693f8f3bc3b8f56ea895b8a74f5ff34b2424
Thomas Pokorny <pokornyth@gmail.com>
2020-10-21 23:47:33 +0200

    logic finished, results get printed to console

--- js.to.pojo.js
+++ js.to.pojo.js
@@ -20,8 +20,24 @@ function toPojo(jsonString, identifier) {
             console.log('\tpublic Integer '  + key + ';');
         } else
         if (Array.isArray(obj[key])) {
-            console.log('\tpublic List<Object> '  + key + ';');
+
+            if((obj[key]).length == 0 ) {
+                console.log('\tpublic List<Object> '  + key + ';');
+            } else 
+            if(typeof (obj[key])[0] == 'string') {        
+                console.log('\tpublic List<String> '  + key + ';');
+            } else 
+            if(typeof (obj[key])[0] == 'number') {        
+                console.log('\tpublic List<Integer> '  + key + ';');
+            } else {
+                console.log('\tpublic List<' + capitalize(key) + '> '  + key + ';');
+                subClasses.push({fieldName: key, val:obj[key][0] });
+            }
+
         }  else 
+        if(obj[key] == null) {
+            console.log('\tpublic Object '  + key + ';');
+        } else 
         {
             console.log('\tpublic ' + capitalize(key) + ' '  + key + ';');
             subClasses.push({fieldName: key, val:obj[key]});
@@ -41,25 +57,6 @@ const capitalize = (s) => {
     return s.charAt(0).toUpperCase() + s.slice(1)
   }
 
-function determinFields(jsonString) {
-    let obj = JSON.parse(jsonString);
-    let fields = { stdField: [], arrayFields: [], objectFields: [] };
-
-    Object.keys(obj).forEach(function(key) {
-        if(typeof obj[key] == 'string' || typeof obj[key] == 'number') {
-            fields.stdField.push({fieldName: key, type: typeof obj[key]});
-        } else
-        if (Array.isArray(obj[key])) {
-            fields.stdField.push({fieldName: key, type: typeof obj[key], val:obj[key]});
-        }  else 
-        {
-            fields.stdField.push({fieldName: key, type: typeof obj[key], val:obj[key]});
-        }
-    });
-
-    return fields;
-}
-
-let json = '{"menu": "we", "something": {"a1":12 , "a2":"sd"} }';
+let json = '{"menu": "we", "something": {"a1":12 , "a2":"sd"}, "hello":null , "arr": [1,2,3,4], "names": ["1","2","3","4"], "geeks":[{"c":"", "c1":""} ,{"c":"", "c1":""} ] }';
 toPojo(json, 'RootElement');
 //console.log(fields);
